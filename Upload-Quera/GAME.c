d16f7608cfe2506af7a34a0b9e9acf5ce96b9889
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-01-01 23:40:27 +0330

    Upload-Finished-Files - version 1.6

---
+++ Upload-Quera/GAME.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
+
+#define SIZE 17
+
+#include "faze1.c"
+extern char Board[SIZE][SIZE];
+
+typedef HANDLE Handle;
+typedef CONSOLE_SCREEN_BUFFER_INFO BufferInfo;
+typedef WORD Word;
+
+short setTextColor(const ConsoleColors foreground);
+
+int player1_row = 0, player1_col = 8, player2_row = SIZE - 1, player2_col = 8;
+char player1_sign = '$', player2_sign = '#';
+int wall1 = 10, wall2 = 10;
+int direction;
+
+void initializeBoard();
+
+void printBoard();
+
+int isValidMove(int way, int x, int y) {
+    if (way == 1 && x >= 0 && x < SIZE && y >= 0 && y < SIZE && Board[x - 1][y] != '=') return 1;
+    else if (way == 2 && x >= 0 && x < SIZE && y >= 0 && y < SIZE && Board[x + 1][y] != '=') return 1;
+    else if (way == 3 && x >= 0 && x < SIZE && y >= 0 && y < SIZE && Board[x][y + 1] != '|') return 1;
+    else if (way == 4 && x >= 0 && x < SIZE && y >= 0 && y < SIZE && Board[x][y - 1] != '|') return 1;
+    else return 0;
+}
+
+void move(int player, int x, int y, int direction) {
+    if (player == 1 && isValidMove(direction, x, y)) {
+        Board[player1_row][player1_col] = ' ';
+        if (direction == 1 && isValidMove(direction, x - 2, y))player1_row -= 2;
+        else if (direction == 2 && isValidMove(direction, x + 2, y)) player1_row += 2;
+        else if (direction == 3 && isValidMove(direction, x, y + 2)) player1_col += 2;
+        else if (direction == 4 && isValidMove(direction, x, y - 2)) player1_col -= 2;
+        Board[player1_row][player1_col] = player1_sign;
+    } else if (player == 2 && isValidMove(direction, x, y)) {
+        Board[player2_row][player2_col] = ' ';
+        if (direction == 1 && isValidMove(direction, x - 2, y)) player2_row -= 2;
+        else if (direction == 2 && isValidMove(direction, x + 2, y)) player2_row += 2;
+        else if (direction == 3 && isValidMove(direction, x, y + 2)) player2_col += 2;
+        else if (direction == 4 && isValidMove(direction, x, y - 2)) player2_col -= 2;
+        Board[player2_row][player2_col] = player2_sign;
+    }
+}
+
+void buildWall(int player, int x, int y, char manner) {
+    if (player == 1 && wall1 > 0) {
+        if (manner == 'V') Board[x * 2 - 2][2 * y - 1] = '|';
+        else
+        {
+            Board[2 * x - 1][2 * y - 2] = '=';
+            Board[2 * x - 1][2 * y] = '=';
+        }
+        wall1--;
+    } else if (player == 2 && wall2 > 0) {
+        if (manner == 'V') Board[x * 2 - 2][2 * y - 1] = '|';
+        else
+        {
+            Board[2 * x - 1][2 * y - 2] = '=';
+            Board[2 * x - 1][2 * y] = '=';
+        }
+        wall2--;
+    }
+}
+
+int checkWinner() {
+    if (player1_row == SIZE - 1) return 1;
+    else if (player2_row == 0) return 2;
+    return 0;
+}
+
+void play(int turn) {
+    int choice, x, y;
+    char manner;
+
+    if (turn == 1) {
+        setTextColor(GREEN);
+
+        printf("\nPlayer 1 turn --> Move(1) or Wall(2): ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            setTextColor(RED);
+
+            printf("Play with Arrows keys!\n");
+            if (getch() == 224) {
+                switch(getch()) {
+                    case 72:
+                        direction = 1;
+                        break;
+                    case 80:
+                        direction = 2;
+                        break;
+                    case 77:
+                        direction = 3;
+                        break;
+                    case 75:
+                        direction = 4;
+                        break;
+                }
+            }
+            move(1, player1_row, player1_col, direction);
+        } else {
+            setTextColor(BLUE);
+
+            printf("Enter the manner (V/H): ");
+            scanf(" %c", &manner);
+
+            manner = toupper(manner);
+
+            setTextColor(LIGHT_AQUA);
+
+            printf("Enter the coordinates (x,y) ([1-9],[1-9]): ");
+            scanf("%d %d", &x, &y);
+
+            buildWall(1, x, y, manner);
+        }
+    } else {
+        setTextColor(GREEN);
+
+        printf("\nPlayer 2 turn --> Move(1) or Wall(2): ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            setTextColor(RED);
+
+            printf("Play with Arrows keys!\n");
+            if (getch() == 224) {
+                switch(getch()) {
+                    case 72:
+                        direction = 1;
+                        break;
+                    case 80:
+                        direction = 2;
+                        break;
+                    case 77:
+                        direction = 3;
+                        break;
+                    case 75:
+                        direction = 4;
+                        break;
+                }
+            }
+             
+            move(2, player2_row, player2_col, direction);
+        } else {
+            setTextColor(BLUE);
+
+            printf("Enter the manner (V/H): ");
+            scanf(" %c", &manner);
+
+            manner = toupper(manner);
+
+            setTextColor(LIGHT_AQUA);
+
+            printf("Enter the coordinates (x,y) ([1-9],[1-9]): ");
+            scanf("%d %d", &x, &y);
+
+            buildWall(2, x, y, manner);
+        }
+    }
+
+    printBoard();
+}
+
+int main() {
+    setTextColor(LIGHT_AQUA);
+    printf("Welcome to Quoridor!\n");
+
+    setTextColor(LIGHT_RED);
+    printf("Sign of player1 is (%c)\n", player1_sign);
+    printf("Sign of player2 is (%c)\n", player2_sign);
+    printf("\n");
+
+    initializeBoard();
+    
+    Board[player1_row][player1_col] = player1_sign;
+    Board[player2_row][player2_col] = player2_sign;
+
+    printBoard();
+
+    while (checkWinner() == 0)
+    {
+        play(1);
+        if (checkWinner() == 1) {
+            printf("Player 1 won!\n");
+            exit(1);
+        }
+        else if (checkWinner() == 2) {
+            printf("Player 2 won!\n");
+            exit(1);
+        }
+        play(2);
+    }
+
+    return 0;
+}
